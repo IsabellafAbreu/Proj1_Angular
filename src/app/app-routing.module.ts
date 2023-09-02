ed756e0d1e5ea4d1a8123735c1e5edde0cbb3be5
IsabellafAbreu <isabella.f.abreu18@gmail.com>
2023-09-01 23:46:29 -0300

    Meu primeiro projeto Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+
+{path:'',component:HomeComponent},
+{path:'galeria',component:GaleriaComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
